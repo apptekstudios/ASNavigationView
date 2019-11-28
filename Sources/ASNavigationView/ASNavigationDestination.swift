8498e56fd69315f23b57b24802230d3b46ec30cf
ApptekStudios <contact@apptekstudios.com>
2019-11-28 15:39:07 +1000

    Fix init error

--- Sources/ASNavigationView/ASNavigationDestination.swift
+++ Sources/ASNavigationView/ASNavigationDestination.swift
@@ -6,7 +6,7 @@ public struct ASNavigationDestination<Content: View> {
 	var screenName: String?
 	var content: Content
 	
-	internal init(id: UUID?, screenName: String? = nil, content: Content) {
+	internal init(id: UUID? = UUID(), screenName: String? = nil, content: Content) {
 		self.id = id
 		self.screenName = screenName
 		self.content = content
